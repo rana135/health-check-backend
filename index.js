a1e3e3eb4a079c096cbc8b25c6941501af679084
rana135 <rana286090@gmail.com>
2023-03-06 20:14:28 +0600

    comment delete

--- index.js
+++ index.js
@@ -15,22 +15,6 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7j04p.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// const verifyJWT = (req, res, next) => {
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//         return res.status(401).send({ message: "UnAuthorized access" })
-//     }
-//     const token = authHeader.split(' ')[1];
-//     console.log(token);
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-//         if (err) {
-//             return res.status(403).send({ message: "Forbidden access" })
-//         }
-//         req.decoded = decoded;
-//         next();
-//     });
-// }
-
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
